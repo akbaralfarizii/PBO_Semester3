426b48f4bd7f6d0619f53537f6073b0efbd1e348
akbaralfarizii <akbargates21@gmail.com>
2021-11-12 02:36:36 +0700

    add Week8

---
+++ Week8/Latihan7_3/Commission.java
@@ -0,0 +1,28 @@
+package Latihan7_3;
+
+public class Commission extends Hourly{
+    private double totalSales;
+    private double commissionRate;
+	
+    public Commission (String eName,String eAddress, String ePhone, String socSecNumber,
+    double rate,double comRate) {
+	super(eName, eAddress, ePhone, socSecNumber, rate);
+	commissionRate = comRate;
+    }
+	
+    public void addSales(double totalSales){
+        this.totalSales = totalSales*commissionRate;
+    }
+    
+    public double pay(){
+        double payment = super.pay() + totalSales;
+        totalSales = 0;
+        return payment;
+    }
+    
+    public String toString(){
+        String result = super.toString();
+        result += "\nTotal Sales: " + totalSales;
+        return result;
+    }
+}
